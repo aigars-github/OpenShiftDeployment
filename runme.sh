a664d996b8d935d5074b2861f497c7e2a75d1467
aigars-github <31169994+aigars-github@users.noreply.github.com>
2018-09-28 19:04:19 +0300

    Update runme.sh
    one command

--- runme.sh
+++ runme.sh
@@ -4,8 +4,10 @@ if [ -f "./hosts" ]; then
    sudo sudo ansible-playbook  -i ./hosts /usr/share/ansible/openshift-ansible/playbooks/adhoc/uninstall.yml
    sudo ansible nodes -a "rm -rf /etc/origin"
    sudo ansible nfs -a "rm -rf /srv/nfs/*"
-   rm -rf hosts
+   rm -rf *
 fi
 export GUID=`hostname|awk -F. '{print $2}'`
+git clone https://github.com/aigars-github/OpenShiftDeployment.git .
+rm -rf .git
 cat hosts.GUID | sed  s/GUID/$GUID/g > hosts
 sudo ansible-playbook  -i ./hosts -f 20 -e "GUID=$GUID" playbook.yml 
